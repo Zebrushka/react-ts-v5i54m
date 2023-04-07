6a3e088ffe282e27d9425f0c06743facbe8e26ea
Zebrushka <44111753+Zebrushka@users.noreply.github.com>
2023-04-07 10:40:54 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import './style.scss';
+import logo from './img/logo.svg';
+import cart from './img/cart.svg';
+import user from './img/user.svg'
+
+
+export default function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img src={logo} width={40} height={40} />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кросовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img src={cart} width={18} height={18} />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+          <img src={user} width={18} height={18} />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+        <div className="Card">
+        <img src="" alt=""/>
+          <p></p>
+          <div>
+            <div>
+              <span>
+                Цена
+              </span>
+              <b>12999 р</b>
+            </div>
+            <button>
+              <img src={user} width={11} height={11} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-v5i54m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-v5i54m)
